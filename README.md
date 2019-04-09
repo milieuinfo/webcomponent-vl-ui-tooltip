99b79d1ece930405037cf7e7764079c45f533491
driesva <Dries.VanAutreve@telenet.be>
2019-04-09 08:38:11 +0200

    APOCOW-8 vl-ui-tooltip - initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# webcomponent-vl-ui-tooltip
-"vl-ui-tooltip" UI component van Webuniversum Vlaanderen als Web Component
+# Webcomponent vl-ui-tooltip
+De ["vl-ui-tooltip" UI component van Webuniversum Vlaanderen](https://overheid.vlaanderen.be/webuniversum/v3/documentation/js-components/vl-ui-tooltip/) als Web Component
+
+Passen VO styling toe.
+
+## Installatie
+```
+npm install --save vl-ui-tooltip
+```
+
+## Demo
+```
+npm run demo
+```
+
+## Credits
+Zie de lijst van [ontwikkelaars](https://github.com/milieuinfo/webcomponent-vl-ui-tooltip/graphs/contributors) die meegewerkt hebben aan de webcomponent.
+
+## Contact
+Mail je suggesties, opmerkingen of tips naar [help@omgevingvlaanderen.be](mailto:help@omgevingvlaanderen.be)
---
+++ vl-tooltip.src.js
@@ -0,0 +1,74 @@
+import { VlElement } from '/node_modules/vl-ui-core/vl-core.js';
+
+(() => {
+  addScript('vl-util.js', '/node_modules/@govflanders/vl-ui-util/dist/js/util.js');
+  addScript('tooltip.js', '/node_modules/tooltip.js/dist/umd/tooltip.js');
+  addScript('popper.js', '/node_modules/popper.js/dist/umd/popper.js');
+  addScript('vl-tooltip.js', '/node_modules/@govflanders/vl-ui-tooltip/dist/js/tooltip.js');
+
+  function addScript(id, src) {
+    if (!document.head.querySelector('#' + id)) {
+      var script = getScript(id, src);
+      document.head.appendChild(script);
+    }
+  }
+
+  function getScript(id, src) {
+    var script = document.createElement('script');
+    script.setAttribute('src', src);
+    return script;
+  }
+})();
+
+/**
+ * vl-tooltip
+ *
+ * @demo demo/vl-tooltip.html
+ */
+export class VlTooltip extends VlElement(HTMLElement) {
+  constructor() {
+    super(`
+            <style>
+                @import "../style.css";
+            </style>
+        `);
+  }
+
+  static get _observedAttributes() {
+    return ['static'];
+  }
+
+  get _classPrefix() {
+    return 'vl-tooltip';
+  }
+
+
+  get _tooltipElement() {
+    return this._element.querySelector('.vl-tooltip');
+  }
+
+  _getTooltipTemplate(newValue) {
+    return this._template(`
+        <div class="vl-tooltip vl-tooltip--static" x-placement="top">
+          <div class="vl-tooltip__inner">
+            <slot></slot>
+          </div>
+          <div class="vl-tooltip__arrow"></div>
+        </div>
+     `);
+  };
+
+  _staticChangedCallback(oldValue, newValue) {
+    if (this._tooltipElement) {
+      this._tooltipElement.remove();
+    }
+
+    if (newValue !== undefined) {
+      const tooltipTemplate = this._getTooltipTemplate();
+      this._element.appendChild(tooltipTemplate);
+    }
+  }
+
+}
+
+customElements.define('vl-tooltip', VlTooltip);
